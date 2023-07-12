f40852f6ac616d4930ca5dd492eed9f4438c241b
oreo <120244424+horimiyaa@users.noreply.github.com>
2023-07-13 03:40:46 +0700

    refactor: add types

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+import {
+  APIApplicationCommandInteraction,
+  APIInteraction,
+  InteractionType
+} from "discord_api_types";
+
+export interface ExecuteOptions {
+  requestEvent: Deno.RequestEvent;
+  interaction: APIInteraction;
+}
+
+export interface CommandExecuteOptions extends Omit<ExecuteOptions, "interaction"> {
+  interaction: APIApplicationCommandInteraction
+}
+
+export interface Event {
+  type: InteractionType;
+  execute: (data: ExecuteOptions) => Promise<void>;
+}
