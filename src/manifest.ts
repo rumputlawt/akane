ef3ca5dcfe2c4f158f6311503d72079fdfb9d632
oreo <120244424+horimiyaa@users.noreply.github.com>
2023-07-18 13:05:20 +0700

    style: update types export style

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,5 +1,6 @@
+import type {
+  Event
+} from "./types.ts";
 import * as eventPing from "./events/ping.ts";
 
-export default {
-  events: [eventPing]
-}
+export const events: Event[] = [eventPing];
