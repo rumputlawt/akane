1350f933e070a101a8b499ecfc61fba19d9e6cd2
oreo <120244424+horimiyaa@users.noreply.github.com>
2023-07-22 04:19:25 +0700

    add `commands` variable manifest

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,6 +1,8 @@
 import type {
+  ChatInputCommand,
   EventPing
 } from "./types.ts";
 import * as eventPing from "./events/ping.ts";
 
+export const commands: (ChatInputCommand)[] = [];
 export const events: (EventPing)[] = [eventPing];
