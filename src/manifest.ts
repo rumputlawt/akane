42ead346c2c15cc265308a84150cddb275d4ee3d
oreo <120244424+horimiyaa@users.noreply.github.com>
2023-07-22 15:15:25 +0700

    export `customRoleCommand` to manifest

--- src/manifest.ts
+++ src/manifest.ts
@@ -2,7 +2,9 @@ import type {
   ChatInputCommand,
   PingEvent
 } from "./types.ts";
+
+import * as customRoleCommand from "./commands/custom-role.ts";
 import * as eventPing from "./events/ping.ts";
 
-export const commands: (ChatInputCommand)[] = [];
+export const commands: (ChatInputCommand)[] = [customRoleCommand];
 export const events: (PingEvent)[] = [eventPing];
