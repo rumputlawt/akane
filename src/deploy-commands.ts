062bb3e0f4051d711903b7b87e822a53b7c7b1d7
oreo <120244424+horimiyaa@users.noreply.github.com>
2023-07-22 15:45:19 +0700

    create deploy commands file

---
+++ src/deploy-commands.ts
@@ -0,0 +1,19 @@
+import {
+  RouteBases,
+  Routes
+  type RESTPutAPIApplicationCommandsJSONBody
+} from "discord_api_types";
+import { 
+  commands as manifestCommands
+} from "./manifest";
+
+const commands: RESTPutAPIApplicationCommandsJSONBody = manifestCommands.map(ctx => ctx.data);
+
+const deploy = await fetch(RouteBases.api + Routes.applicationCommands(Deno.env.get("DISCORD_ID")), {
+  headers: new Headers(
+    { "authorization": Deno.env.get("DISCORD_TOKEN") }
+  ),
+  body: JSON.stringify(commands)
+})
+  .then(res => res.json());
+console.log(deploy);
